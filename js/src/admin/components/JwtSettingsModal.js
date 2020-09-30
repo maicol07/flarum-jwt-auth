83d399a5d07b3049c78a38372d1146080156c4b7
Maicol <maicolbattistini@live.it>
2020-09-30 14:48:12 +0200

    refactor: ♻️ Changed namespace

--- js/src/admin/components/JwtSettingsModal.js
+++ js/src/admin/components/JwtSettingsModal.js
@@ -6,14 +6,14 @@ export default class JwtSettingsModal extends SettingsModal {
   }
 
   title() {
-    return app.translator.trans('coldsnake-jwt-auth.admin.jwt_settings.title');
+    return app.translator.trans('maicol07-jwt-auth.admin.jwt_settings.title');
   }
 
   form() {
     return [
       <div className="Form-group">
-        <label>{app.translator.trans('coldsnake-jwt-auth.admin.jwt_settings.iss_label')}</label>
-        <input className="FormControl" bidi={this.setting('coldsnake-jwt-auth.iss')}/>
+        <label>{app.translator.trans('maicol07-jwt-auth.admin.jwt_settings.iss_label')}</label>
+        <input className="FormControl" bidi={this.setting('maicol07-jwt-auth.iss')}/>
       </div>
     ];
   }
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,4 +1,3 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 
 import JwtSettingsModal from './components/JwtSettingsModal';
