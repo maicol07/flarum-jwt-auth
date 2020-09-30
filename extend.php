87314efb482132b9b3c2e9a5a8cb361f80995e6e
Maicol <maicolbattistini@live.it>
2020-09-30 12:59:59 +0200

    refactor: ♻️ Changed namespace

--- extend.php
+++ extend.php
@@ -7,5 +7,5 @@ return [
     (new Extend\Frontend('admin'))
             ->js(__DIR__.'/js/dist/admin.js'),
     (new Extend\Routes('api'))
-        ->get('/jwt-auth', 'coldsnake.jwt-auth', JwtAuthController::class)
+        ->get('/jwt-auth', 'maicol07.jwt-auth', JwtAuthController::class)
 ];
